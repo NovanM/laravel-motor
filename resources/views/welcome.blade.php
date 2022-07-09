74afdd45d7d57c3f7b04fb02aea5614ae9496e7d
NovanM <azizinovanm@gmail.com>
2022-07-09 16:59:34 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                GO Motor Service
                 </div>
 
                 <div class="links">

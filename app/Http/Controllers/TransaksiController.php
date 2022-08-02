67132ef383a699219d14861860b985ea54ec1569
NovanM <azizinovanm@gmail.com>
2022-08-03 01:26:42 +0700

    update

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -14,7 +14,7 @@ class TransaksiController extends Controller
     //
     public function index()
     {
-        $pagename = 'Data Laporan Transaksi';
+        $pagename = 'Data Laporan Keuangan';
         $allTransaksi = Transaksi::orderBy('created_at','desc')->get();
         $total = Transaksi::sum('total');
 

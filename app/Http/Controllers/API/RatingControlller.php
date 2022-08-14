61b3e53e3a1e5a55a0a7574bf51a9dbd993b45ea
NovanM <azizinovanm@gmail.com>
2022-08-14 11:36:34 +0700

    update

--- app/Http/Controllers/API/RatingControlller.php
+++ app/Http/Controllers/API/RatingControlller.php
@@ -17,7 +17,7 @@ class RatingControlller extends Controller
     {
 
         if ($id) {
-            $rating = Rating::where('transaksi_id', $id)->first();
+            $rating = Rating::where('transaksi_id', $id)->with(['transaksi','status.user'])->first();
         }else if ($id==0){
             // $rating = Rating::where('user_id', Auth::user()->id)->get();
             $rating = Rating::with(['transaksi','status.user'])->get();
